5ba6f2537100ae537065806083dd3a18699873e5
KhuriaKhusna <90915628+KhuriaKhusna@users.noreply.github.com>
2022-11-23 14:11:32 +0700

    Update Player.java

--- src/com/javagame/sprite/Player.java
+++ src/com/javagame/sprite/Player.java
@@ -1,5 +1,26 @@
 package com.javagame.sprite;
 
-public class Player extends Sprite{
+public class Player extends Sprite {
+  
+  private int width;
 
+    public Player() {
+
+        initPlayer();
+    }
+  
+  public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+
+            dx = -2;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+
+            dx = 2;
+        }
+    }
 }
